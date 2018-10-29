16c2fd65da0c4b8066db8d4061dbc094f0c58fe6
alumno <alumno@aula1-07>
2018-10-29 18:21:06 +0100

    Inicio proyecto

---
+++ Ahorcado2/src/ahorcado2/Ahorcado2.java
@@ -0,0 +1,16 @@
+package ahorcado2;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ahorcado2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Gint");
+    }
+    
+}
